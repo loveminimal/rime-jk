49e9c66fe02e16f0054aa4ebbfa8d698a8246af1
jack <loveminimal@163.com>
2025-01-11 19:04:26 +0800

    perf: dicts|symbols update

--- scripts/header.py
+++ scripts/header.py
@@ -26,7 +26,40 @@ def get_header(file_name):
 # 
 ---
 name: {'.'.join(file_name.split('.')[:-2])}
-version: {datetime.now().date()}
+version: '{datetime.now().date().strftime("%Y.%m")}'
+sort: by_weight
+use_preset_vocabulary: false
+...
+'''
+    return header.strip() + '\n'
+
+
+# === 中文表头 ===
+def get_header_ice_wx(file_name):
+    header = f'''
+# Rime dictionary - {file_name}
+# encoding: utf-8
+# 
+# --- 说明 ---
+# 该字典是基于白霜拼音结合万象词库转换而成
+# - https://github.com/loveminimal/rime-jk
+# - Jack Liu <https://aituyaa.com>
+# 
+# 修改内容：
+# - 删除非国标 8105-2023 单字及其所组词语
+# 
+# 运行脚本：
+# - https://github.com/loveminimal/rime-utils/blob/master/scripts/py2wx_8105.py
+# - https://github.com/loveminimal/rime-utils/blob/master/scripts/wx2custom_8105.py
+# 
+# 参考码表：
+# - https://github.com/iDvel/rime-ice
+# - https://github.com/gaboolic/rime-frost 
+# - https://github.com/amzxyz/rime_wanxiang_pro
+# 
+---
+name: {'.'.join(file_name.split('.')[:-2])}
+version: '{datetime.now().date().strftime("%Y.%m")}'
 sort: by_weight
 use_preset_vocabulary: false
 ...
@@ -35,6 +68,7 @@ use_preset_vocabulary: false
 
 
 # === 用户词典同步表头 ===
+# --- 用户词典同步表头·五笔86 ---
 def get_header_sync(file_name):
     header = f'''
 # Rime dictionary - {file_name}
@@ -46,18 +80,41 @@ def get_header_sync(file_name):
 # - Jack Liu <https://aituyaa.com>
 # 
 # 运行脚本：
-# - https://github.com/loveminimal/rime-jk/blob/master/scripts/sync_wubi_user_dict.py
-# - py scripts/rime_sync_dict_to_wubi86_user_dict.py [-i src] [-o out] [-f file_endswith_filter] [-m multifile_out_mode]
+# - https://github.com/loveminimal/rime-utils/blob/master/scripts/sync_wubi_user_dict.py
+# - py scripts/sync_wubi_user_dict.py [-i src] [-o out] [-f file_endswith_filter] [-m multifile_out_mode]
 # 
 ---
 name: {'.'.join(file_name.split('.')[:-2])}
-version: {datetime.now().date()}
+version: '{datetime.now().date().strftime("%Y.%m")}'
 sort: by_weight
 use_preset_vocabulary: false
 ...
 '''
     return header.strip() + '\n'
 
+# --- 用户词典同步表头·拼音+五笔前二 ---
+def get_header_sync_py(file_name):
+    header = f'''
+# Rime dictionary - {file_name}
+# encoding: utf-8
+# 
+# --- 说明 ---
+# 该字典是基于官方自动同步的用户词典合并排序生成
+# - https://github.com/loveminimal/rime-jk
+# - Jack Liu <https://aituyaa.com>
+# 
+# 运行脚本：
+# - https://github.com/loveminimal/rime-utils/blob/master/scripts/sync_py_user_dict.py
+# - py scripts/sync_py_user_dict.py [-i src] [-o out] [-f file_endswith_filter] [-m multifile_out_mode]
+# 
+---
+name: {'.'.join(file_name.split('.')[:-2])}
+version: '{datetime.now().date().strftime("%Y.%m")}'
+sort: by_weight
+use_preset_vocabulary: false
+...
+'''
+    return header.strip() + '\n'
 
 
 # === 英文表头 ===
@@ -67,8 +124,10 @@ def get_en_header(file_name):
 # encoding: utf-8
 # 
 # --- 说明 ---
-# 该字典是基于雾淞版本和黄狗超大字符集英文码表合并排序生成
-# - 按字母长度数进行排序处理
+# 该字典是基于雾淞版本、Easy English Nano 和黄狗超大字符集英文码表合并排序生成
+# - ¹按字母长度数进行排序处理
+# - ²按字母 A-Za-z 顺序排序，不区分大小写
+# - ³将字母编码部分全部转为小写并擦去特殊符号，如 -': 等
 # - https://github.com/loveminimal/rime-jk
 # - Jack Liu <https://aituyaa.com>
 # 
@@ -78,12 +137,40 @@ def get_en_header(file_name):
 # 
 # 参考码表：
 # - https://github.com/iDvel/rime-ice
+# - https://github.com/tumuyan/rime-melt
 #
 ---
 name: {'.'.join(file_name.split('.')[:-2])}
-version: {datetime.now().date()}
+version: '{datetime.now().date().strftime("%Y.%m")}'
 sort: by_weight
 use_preset_vocabulary: false
 ...
+'''
+    return header.strip() + '\n'
+
+
+def get_en_aliases_header(file_name):
+    header = f'''
+# Rime dictionary - {file_name}
+# encoding: utf-8
+# 
+# --- 说明 ---
+# 该字典是对既有英文词典的个人扩充
+# 用于自定义字符串、命令的快捷输入
+# 转化自个人使用的 .bash_aliases 脚本别名文件
+# - https://github.com/loveminimal/rime-jk
+# - Jack Liu <https://aituyaa.com>
+# 
+# 运行脚本：
+# - https://github.com/loveminimal/rime-utils/blob/master/scripts/bash_alias2en_aliases_dict.py
+# - py scripts/wubi86.py [-i src] [-o out] [-f file_endswith_filter] [-m multifile_out_mode]
+#
+---
+name: {'.'.join(file_name.split('.')[:-2])}
+version: '{datetime.now().date().strftime("%Y.%m")}'
+sort: by_weight
+use_preset_vocabulary: false
+...
+
 '''
     return header.strip() + '\n'
--- scripts/update-flypy_dicts.sh
+++ scripts/update-flypy_dicts.sh
@@ -1,3 +1,10 @@
+# update-flypy_dicts.sh
+# by Jack Liu <https://aituyaa.com>
+# 用来同步转换最新的白霜拼音词库为小鹤双拼+五笔前二词库
+# 
+CURRENT_DIR=$(pwd)
+OUT_DICTS='flypy_dicts'
+
 cd D:/sourcecode/sc_rime/rime-frost
 echo -e "\e[32m🔜 正在拉取白霜拼音词库最近更新 »»»»»»»»»»»»»»»»\e[0m"
 git pull
@@ -8,20 +15,21 @@ echo -e "\e[32m🔜 正在转换为白霜万象词库      「 cn_dicts_wx 」\e
 py scripts/py2wx_8105.py
 echo -e "\e[42m✅ 白霜万象词库已转换成功      「 cn_dicts_wx 」\e[0m"
 
-echo -e "\e[32m🔜 正在转换为白霜万象鹤形词库  「 flypy_dicts 」\e[0m"
+echo -e "\e[32m🔜 正在转换为白霜万象五笔词库  「 $OUT_DICTS 」\e[0m"
 py scripts/wx2custom_8105.py
-echo -e "\e[42m✅ 白霜万象鹤形词库已转换完成  「 flypy_dicts 」\e[0m"
+echo -e "\e[42m✅ 白霜万象鹤形词库已转换完成  「 $OUT_DICTS 」\e[0m"
 
 cd C:/Users/jack/AppData/Roaming/Rime
-echo -e "\e[32m🔜 开始复制为白霜万象鹤形词库  「 flypy_dicts 」\e[0m"
-echo -e "正在复制 20% »»»»                          「 flypy_dicts 」"
-echo -e "正在复制 63% »»»»»»»»»»»»»»»»              「 flypy_dicts 」"
-echo -e "正在复制 99% »»»»»»»»»»»»»»»»»»»»»»»»»»»»»»「 flypy_dicts 」"
-if [ -d "flypy_dicts" ]
+echo -e "\e[32m🔜 开始复制为白霜万象五笔词库  「 $OUT_DICTS 」\e[0m"
+echo -e "正在复制 20% »»»»                          「 $OUT_DICTS 」"
+echo -e "正在复制 63% »»»»»»»»»»»»»»»»              「 $OUT_DICTS 」"
+echo -e "正在复制 99% »»»»»»»»»»»»»»»»»»»»»»»»»»»»»»「 $OUT_DICTS 」"
+if [ -d "$OUT_DICTS" ]
 then
-    rm -rf flypy_dicts
+    rm -rf $OUT_DICTS
 fi
-cp -r D:/sourcecode/sc_rime/rime-utils/dicts/rime-frost/flypy_dicts ./
-echo -e "\e[42m✅ 白霜万象鹤形词库已复制完成  「 flypy_dicts 」\e[0m"
+cp -r D:/sourcecode/sc_rime/rime-utils/dicts/rime-frost/$OUT_DICTS ./
+echo -e "\e[42m✅ 白霜万象鹤形词库已复制完成  「 $OUT_DICTS 」\e[0m"
 
 # cd D:/sourcecode/sc_rime/rime-utils
+cd $CURRENT_DIR

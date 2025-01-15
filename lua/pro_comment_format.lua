cdc679204c7930a7385cf39fb626da9ba48d2f3e
jack <loveminimal@163.com>
2025-01-15 13:47:49 +0800

    fix: exclude en_aliases

--- lua/pro_comment_format.lua
+++ lua/pro_comment_format.lua
@@ -113,9 +113,20 @@ function C.func(input, env)
             -- 增强一下英文词典反查 将释义放在注释中---
             local cand_text = cand.text
             -- log.warning('cand.text >>> ' .. cand_text) 
+
+            -- 排除包含路径字符或命令符号的字符串 - 全部放候选
+            -- 此类字符基本源自 en_aliases 的命令别名
+            -- 如 cd ~/AppData/Roaming/Rime && source scripts/update-cn_dicts_wx.sh
+            if string.find(cand_text, "^[%a%d%&%-%_%~%/%.%s%:%=%*]+$") then
+                yield(cand)
+                goto continue
+            end
+
+            -- 正确格式输出英文词典 - 候选 + 释义注释
             -- local cand_text = "China n.    中国 adj. 中国的 中国制造的"
             -- local word, pos, meaning = string.match(cand_text, "(%a+)%s+(%a+%.%s*)(.+)")
             local word, pos, meaning = string.match(cand_text, "(%a+)%s+([%a&%.]+)%s+(.+)")
+
             if word then                
                 -- log.warning("Word:" .. word)        -- 输出: Word: China
                 -- log.warning("POS:" .. pos)          -- 输出: POS: n.    

e5db941f62dc7b3b071146b8b0ea7465a647a423
jack <loveminimal@163.com>
2025-03-12 14:20:09 +0800

    style: perf output

--- scripts/deploy_rime.py
+++ scripts/deploy_rime.py
@@ -5,7 +5,7 @@ weasel_deployer_path = "C:/Program Files/Rime/weasel-0.16.3/WeaselDeployer.exe"
 
 # 执行部署命令
 def deploy_rime():
-    print('☑️  === 重新部署 Rime 最新配置 ===')
+    print('\n☑️  === 重新部署 Rime 最新配置 ===')
     try:
         result = subprocess.run([weasel_deployer_path, "/deploy"], capture_output=True, text=True)
         if result.returncode == 0:
--- scripts/sync_rime.py
+++ scripts/sync_rime.py
@@ -5,7 +5,7 @@ weasel_deployer_path = "C:/Program Files/Rime/weasel-0.16.3/WeaselDeployer.exe"
 
 # 执行同步命令
 def sync_rime():
-    print('☑️  === 开始同步 Rime 最新配置 ===')
+    print('\n☑️  === 开始同步 Rime 最新配置 ===')
     try:
         result = subprocess.run([weasel_deployer_path, "/sync"], capture_output=True, text=True)
         if result.returncode == 0:

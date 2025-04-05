2af78d1748f790dbf9a9d12dfb05934912a7cae7
jack <loveminimal@163.com>
2025-04-05 16:57:29 +0800

    chore: keep short code first

--- lua/keep_short_code_first_tiger.lua
+++ lua/keep_short_code_first_tiger.lua
@@ -1,5 +1,5 @@
 --[[ 
-keep_secondary_code_first: 保持虎码的二级简码对应的汉字在候选列表首位
+keep_short_code_first_tiger: 保持虎码的一、二级简码对应的汉字在候选列表首位
 by Jack Liu <https://aituyaa.com>
 --]]
 
@@ -63,11 +63,43 @@ local secondary_code_table = {
 }
 
 -- 定义过滤器函数
-function keep_secondary_code_first(input, env)
+function keep_short_code_first_tiger(input, env)
     -- 获取用户输入的编码
     local input_code = env.engine.context.input
+    -- 仅处理单字符输入
+    if #input_code == 1 then
+        -- 从一级简码表中获取对应字符
+        local primary_char = primary_code_table[input_code]
+        if primary_char then
+            local new_candidates = {}
+            local found = false
+            local count = 0
+            -- 遍历前 4 个候选项
+            for cand in input:iter() do
+                count = count + 1
+                if cand.text == primary_char then
+                    -- 若找到一级简码字符，将其置于新候选列表首位
+                    table.insert(new_candidates, 1, cand)
+                    found = true
+                else
+                    -- 其他候选词依次添加到新候选列表
+                    table.insert(new_candidates, cand)
+                end
+                if count >= 4 then
+                    break
+                end
+            end
+            -- 若找到一级简码字符，输出新候选列表
+            if found then
+                for _, cand in ipairs(new_candidates) do
+                    yield(cand)
+                end
+                return
+            end
+        end
+        
     -- 仅处理双字符输入
-    if #input_code == 2 then
+    elseif #input_code == 2 then
         -- 从二级简码表中获取对应字符
         local secondary_char = secondary_code_table[input_code]
         if secondary_char then
@@ -98,11 +130,12 @@ function keep_secondary_code_first(input, env)
             end
         end
     end
-    -- 若未找到二级简码字符或输入非双字符，输出原始候选列表
+
+    -- 若未找到一、二级简码字符或输入非单、双字符，输出原始候选列表
     for cand in input:iter() do
         yield(cand)
     end
 end
 
 -- 导出过滤器函数
-return keep_secondary_code_first
+return keep_short_code_first_tiger

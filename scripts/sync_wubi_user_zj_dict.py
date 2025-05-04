8502dd4de0b72262cec43d8b79d091705ae0bc19
jack <loveminimal@163.com>
2025-05-05 00:26:19 +0800

    perf: dicts|symbols update

--- scripts/sync_wubi_user_zj_dict.py
+++ scripts/sync_wubi_user_zj_dict.py
@@ -55,7 +55,7 @@ def convert(src_dir, out_dir):
                 weight = line_list[2].split(' ')[0].split('=')[1]
 
                 # 删除权重为负数的字词（废词）
-                if int(weight) < 0:
+                if int(weight) <= 0:
                     continue
 
                 # 处理特殊编码

8f2c00cd47024a4cd32584599eb6ed88666b5067
jack <loveminimal@163.com>
2025-06-18 17:24:13 +0800

    perf: dicts|symbols update

--- lua/user_words.lua
+++ lua/user_words.lua
@@ -1,16 +1,22 @@
 -- type: tiger
 local user_words = {
     ["一场梦"] = "fgee",
+    ["出简"] = "smra",
+    ["出简让全"] = "srsj",
+    ["叶凡"] = "dnoi",
     ["有坂"] = "nvgx",
     ["梦中界"] = "edqj",
     ["横跳"] = "enpp",
     ["欧国联"] = "nrqh",
     ["滑跪"] = "kfpx",
     ["爱涂鸦的蜗牛"] = "yklq",
+    ["独体字"] = "mjwh",
     ["知北游"] = "oakl",
     ["神识"] = "fosd",
     ["自我解构"] = "otxe",
+    ["让全"] = "syjn",
     ["通天教主"] = "kfqi",
     ["道祖"] = "hoff",
+    ["鹤形"] = "wufl",
 }
 return user_words

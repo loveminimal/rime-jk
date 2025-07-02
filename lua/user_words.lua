1a378e9fb598a7de527fcbf2199471e51aac4457
jack <loveminimal@163.com>
2025-07-02 10:28:25 +0800

    feat: recovery flyyx

--- lua/user_words.lua
+++ lua/user_words.lua
@@ -1,15 +1,15 @@
--- type: tiger
+-- type: flyyx
 local user_words = {
-    ["候选栏"] = "jqeh",
-    ["候选框"] = "jqen",
-    ["吞噬星空"] = "fdoe",
-    ["周予"] = "mgiv",
-    ["咱孩"] = "dohk",
-    ["姜太虚"] = "gmzc",
-    ["季欢"] = "xhre",
-    ["安印"] = "wbbf",
-    ["宫保"] = "wdjd",
-    ["直聘"] = "nqqd",
-    ["自造词"] = "oqsa",
+    ["候选栏"] = "hxlj",
+    ["候选框"] = "hxkl",
+    ["吞噬星空"] = "tuxk",
+    ["周予"] = "vzyu",
+    ["咱孩"] = "zjhd",
+    ["姜太虚"] = "jtxu",
+    ["季欢"] = "jihr",
+    ["安印"] = "anyb",
+    ["宫保"] = "gsbc",
+    ["直聘"] = "vipb",
+    ["自造词"] = "zzci",
 }
 return user_words

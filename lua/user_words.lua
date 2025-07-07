5677801923f94ec9d39235d2d1784db6d3949fb3
jack <loveminimal@163.com>
2025-07-07 23:22:01 +0800

    perf: dicts|symbols update

--- lua/user_words.lua
+++ lua/user_words.lua
@@ -1,12 +1,5 @@
 -- type: flyyx
 -- top: false
 local user_words = {
-    ["小狼毫"] = "xlhc",
-    ["开发者"] = "kfve",
-    ["极点"] = "jidm",
-    ["移植"] = "yivi",
-    ["线程"] = "xmig",
-    ["芬达"] = "ffda",
-    ["韩老魔"] = "hlmo",
 }
 return user_words

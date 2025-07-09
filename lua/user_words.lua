c5db919a424e9338492f2689fed2bdd3dae9f657
jack <loveminimal@163.com>
2025-07-09 12:53:12 +0800

    perf: dicts|symbols update

--- lua/user_words.lua
+++ lua/user_words.lua
@@ -1,6 +1,10 @@
 -- type: flyyx
 -- top: false
 local user_words = {
+    ["世俗"] = "uisu",
+    ["个人"] = "gerf",
+    ["微博"] = "wwbo",
+    ["清纯"] = "qkiy",
     ["自造词"] = "zzci",
 }
 return user_words
